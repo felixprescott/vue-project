4c6de364b3cfea6c263a0c4f0a9081c1b6fe140a
Sergei Sopochkin <sergej.sopochkin@nutnet.ru>
2023-08-09 12:25:24 +0400

    converting to composition api

--- src/App.vue
+++ src/App.vue
@@ -1,47 +1,82 @@
-<script setup>
-import HelloWorld from './components/HelloWorld.vue'
-import TheWelcome from './components/TheWelcome.vue'
-</script>
-
 <template>
-  <header>
-    <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
-
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
-    </div>
-  </header>
-
-  <main>
-    <TheWelcome />
-  </main>
+  <div>
+    Counter: {{ counter }}
+    <span v-if="counter % 2 === 0">(Odd)</span>
+    <span v-else>(Even)</span>
+  </div>
+  <div>Month name: {{ monthName }}</div>
+  <div>
+    <label for="amount">Amount: </label>
+    <input
+      id="amount"
+      type="number"
+      v-on:change="changeAmount"
+      v-bind:value="amount"
+    />
+    <button @click="inc">inc</button>
+    <button @click="dev">dec</button>
+  </div>
+  <div>
+    <ul>
+      <li v-for="(user, i) in usersList" :key="`user-${i}`">
+        {{ i }}. {{ user }}
+      </li>
+    </ul>
+  </div>
+  <div>New user name: {{ customUser }}</div>
+  <div>
+    <input type="text" v-model="customUser" />
+    <button @click="addUser">add</button>
+  </div>
 </template>
 
-<style scoped>
-header {
-  line-height: 1.5;
-}
+<script setup>
+const MONTHS = [
+  'January',
+  'February',
+  'March',
+  'April',
+  'May',
+  'June',
+  'July',
+  'August',
+  'September',
+  'October',
+  'November',
+  'December',
+];
 
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
+import { ref, reactive, computed } from 'vue';
 
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
+const counter = ref(1);
+const amount = ref(1);
+const customUser = ref(null);
+const usersList = reactive(['Bob', 'Tom', 'John']);
 
-  .logo {
-    margin: 0 2rem 0 0;
-  }
+const changeAmount = (e) => {
+  amount.value = Number(e.target.value);
+};
+const inc = () => {
+  counter.value += amount.value;
+};
+const dev = () => {
+  counter.value -= amount.value;
+};
+const addUser = () => {
+  usersList.push(customUser.value);
+};
 
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
+const monthName = computed(() => {
+  const monthNum = (counter.value - 1) % 12;
+  return MONTHS[monthNum];
+});
+</script>
+
+<style>
+body,
+input,
+button {
+  font-size: 30px;
+  font-family: monospace;
 }
 </style>
